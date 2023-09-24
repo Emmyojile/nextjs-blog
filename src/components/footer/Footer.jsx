ac2997fbc691224c466a8591ede2238f128b5c98
Emmyojile <emmaojile99@gmail.com>
2023-09-23 21:37:04 -0800

    server and client side rendering

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  console.log('hello');
+  return (
+    <div>
+        <div>@2023 EmmyDev. All Rights Reserved</div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
