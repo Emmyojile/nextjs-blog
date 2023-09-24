5c9b5b8b163901ed658e83f042ed73b68d98afee
Emmyojile <emmaojile99@gmail.com>
2023-09-24 13:00:35 -0800

    blog ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+// import Hero from 'public/hero.jpg'
 
 export default function Home() {
   return (
     <div>
-      Home
+      <div></div>
+      <div></div>
+      <Image src="/hero.jpg" width={20} height={20} className={styles.img}/>
     </div>
   )
 }
