e9516cc0a3ca8f6cd5d1b7e808f6abab3ac62402
Emmyojile <emmaojile99@gmail.com>
2023-09-24 12:25:41 -0800

    blog ui

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css'
+import Image from 'next/image';
 
 const Footer = () => {
   console.log('hello');
   return (
-    <div>
+    <div className={styles.container}>
         <div>@2023 EmmyDev. All Rights Reserved</div>
-        <div></div>
+        <div className={styles.socials}>
+          <Image src='/facebook.png' width={20} height={20} className={styles.icon} alt='Emmy Dev'/>
+          <Image src='/linkedin.png' width={20} height={20} className={styles.icon} alt='Emmy Dev'/>
+          <Image src='/twitter.png' width={20} height={20} className={styles.icon} alt='Emmy Dev'/>
+          <Image src='/instagram.png' width={20} height={20} className={styles.icon} alt='Emmy Dev'/>
+        </div>
     </div>
   )
 }
