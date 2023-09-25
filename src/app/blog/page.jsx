0fc424734727e5b59881b6d4385b44fbf6057a0b
Emmyojile <emmaojile99@gmail.com>
2023-09-25 11:59:58 -0800

    blog ui

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,8 +5,38 @@ import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>
-      <Link href="testId">
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Desc</p>
+      </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Desc</p>
+      </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
       <div className={styles.imgContainer}>
         <Image
           src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
