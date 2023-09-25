c5923913a28fd0907204c5cc6171609050d3514b
Emmyojile <emmaojile99@gmail.com>
2023-09-25 09:34:48 -0800

    blog ui

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import styles from './contact.module.css'
+import Image from 'next/image'
+import contactImg from "public/contact.jpg";
+import Button from '@/components/button/Button';
+
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep In Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+          src={contactImg}
+          fill={true}
+          className={styles.img}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder='name' className={styles.input} />
+          <input type="text" placeholder='email' className={styles.input} />
+          <textarea className={styles.textArea} placeholder='message' cols="30" rows="10"></textarea>
+          <Button url="#" text="Send"/>
+        </form>
+      </div>
+    </div>
   )
 }
 
