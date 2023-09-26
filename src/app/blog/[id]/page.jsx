5523fa64271ce7eb1ef98a963df1da1c25905c2f
Emmyojile <emmaojile99@gmail.com>
2023-09-26 15:42:37 -0800

    api and server routes

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store"
   });
   // The return value is *not* serialized
@@ -29,15 +29,12 @@ const BlogPost = async ({params}) => {
             {data.title}
           </h1>
           <p className={styles.desc}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque qui
-            nesciunt ipsa recusandae maxime mollitia molestiae eos, praesentium
-            tempora quidem quis at blanditiis distinctio velit impedit voluptas
-            odio dicta magnam.
+            {data.desc}
           </p>
           <div className={styles.author}>
             <Image
               src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
+              alt="Author image"
               width={40}
               height={40}
               className={styles.avatar}
@@ -47,7 +44,7 @@ const BlogPost = async ({params}) => {
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src='https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=600'
+            src={data.image}
             alt=""
             fill={true}
             className={styles.image}
@@ -57,12 +54,7 @@ const BlogPost = async ({params}) => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat odit
-          molestiae veniam assumenda qui, ipsam laborum nulla! Blanditiis
-          ducimus obcaecati, harum necessitatibus ex esse labore quisquam
-          eligendi quia pariatur maiores eos voluptates, eaque totam facere
-          consequatur. Blanditiis quod, perferendis ipsum vero sunt similique
-          voluptates fugit tempora nostrum? Saepe, nulla inventore.
+        {data.content}
         </p>
       </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store"
   });
   // The return value is *not* serialized
@@ -23,10 +23,10 @@ const Blog = async () => {
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href="/blog/testId" className={styles.container} key={item.id}>
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
           <div className={styles.imgContainer}>
             <Image
-              src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src={item.image}
               alt=""
               width={400}
               height={250}
@@ -35,7 +35,7 @@ const Blog = async () => {
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>Desc</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
