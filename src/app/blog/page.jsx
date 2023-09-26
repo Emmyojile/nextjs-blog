0720298d757116bc14283628e86dd597b4be4e46
Emmyojile <emmaojile99@gmail.com>
2023-09-26 11:06:35 -0800

    data fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,58 +1,46 @@
-import React from 'react'
-import styles from './blog.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import styles from "./blog.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const Blog = () => {
-  return (
-    <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt=""
-          width={400}
-          height={250}
-          className={styles.img}
-        />
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Test</h1>
-        <p className={styles.desc}>Desc</p>
-      </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt=""
-          width={400}
-          height={250}
-          className={styles.img}
-        />
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Test</h1>
-        <p className={styles.desc}>Desc</p>
-      </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt=""
-          width={400}
-          height={250}
-          className={styles.img}
-        />
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Test</h1>
-        <p className={styles.desc}>Desc</p>
-      </div>
-      </Link>
-    </div>
-  )
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store"
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-export default Blog
+const Blog = async () => {
+  const data = await getData();
+  return (
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link href="/blog/testId" className={styles.container} key={item.id}>
+          <div className={styles.imgContainer}>
+            <Image
+              src="https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.img}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
 import styles from './dashboard.module.css'
+import useSWR from 'swr'
 
 const Dashboard = () => {
+  // const [data, setData] = useState([])
+  // const [err, setErr] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true)
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store"
+  //     });
+  //     // The return value is *not* serialized
+  //     // You can return Date, Map, Set, etc.
+    
+  //     if (!res.ok) {
+  //       // This will activate the closest `error.js` Error Boundary
+  //       // throw new Error("Failed to fetch data");
+  //       setErr(true);
+  //     }
+    
+  //     const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false)
+  //   }
+  //   getData();
+  // },[]);
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+ 
+
+  // console.log(data);
   return (
     <div className={styles.container}>Dashboard</div>
   )
