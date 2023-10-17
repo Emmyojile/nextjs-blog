cc039c7431b35bee90982bf324be0417aaa4cba1
Emmyojile <emmaojile99@gmail.com>
2023-10-16 22:18:42 -0800

    authentication routes ad apis

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,27 +1,27 @@
 import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import connectDb from "@/utils/db";
 import User from "@/models/User";
+import connect from "@/utils/db";
 import bcrypt from "bcryptjs";
+import connectDb from "@/utils/db";
 
 const handler = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
     CredentialsProvider({
       id: "credentials",
       name: "Credentials",
       async authorize(credentials) {
+        //Check if the user exists.
         await connectDb();
 
         try {
-          const user = await User.findOne({ email: credentials.email });
+          const user = await User.findOne({
+            email: credentials.email,
+          });
 
           if (user) {
-            //check for password
             const isPasswordCorrect = await bcrypt.compare(
               credentials.password,
               user.password
@@ -30,20 +30,29 @@ const handler = NextAuth({
             if (isPasswordCorrect) {
               return user;
             } else {
-              throw new Error("Wrong Credentials");
+              throw new Error("Wrong Credentials!");
             }
           } else {
-            throw new Error("User Not Found");
+            throw new Error("User not found!");
           }
         } catch (err) {
           throw new Error(err);
         }
       },
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
   pages: {
-    error: "/dashboard/login"
-  }
+    error: "/dashboard/login",
+  },
+
 });
 
 export { handler as GET, handler as POST };
