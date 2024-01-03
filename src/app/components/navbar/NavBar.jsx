748ab91bf4148301941de40507b7d8f495c8bf31
Emmyojile <emmaojile99@gmail.com>
2024-01-03 23:18:42 +0100

    layout and setup

---
+++ src/app/components/navbar/NavBar.jsx
@@ -0,0 +1,10 @@
+import styles from "./navbar.module.css"
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>Navbar</div>
+
+  )
+}
+
+export default NavBar
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/NavBar";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div
+  >
+    <NavBar/>
+    <Footer/>
+</div>;
 }
